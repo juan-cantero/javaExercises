232c0a334c9ece1c698687bd373b400078324510
juanqui1989 <androideva02@gmail.com>
2018-08-31 10:31:30 -0300

    new

---
+++ src/Ex1_1.java
@@ -0,0 +1,11 @@
+
+public class Ex1_1 {
+
+	
+	public static void main(String[] args) {
+		System.out.println("welcome to java");
+		System.out.println("Welcome to computer Science");
+		System.out.println("Programming is fun");
+	}
+
+}
---
+++ src/Ex1_2.java
@@ -0,0 +1,13 @@
+
+public class Ex1_2 {
+
+
+	public static void main(String[] args) {
+		System.out.println("welcome java");
+		System.out.println("welcome java");
+		System.out.println("welcome java");
+		System.out.println("welcome java");
+		System.out.println("welcome java");
+	}
+
+}
